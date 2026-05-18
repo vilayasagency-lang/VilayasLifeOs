4c7e9a7e6aab9f918e27adf8a528aba277e0e51b
vilayasagency-lang <vilayas.agency@gmail.com>
2026-05-18 11:20:07 +0530

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
 // Supabase Configuration
-const SUPABASE_URL = 'https://gjjwtwhdodwjqvyvfnwc.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://gjjwtwhdodwjqvyvfnwc.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdqand0d2hkb2R3anF2eXZmbndjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwMzkwNzcsImV4cCI6MjA5NDYxNTA3N30.nOdOqHubefP6Ai2nGtpzbU6p87YZuKLOHhEXyjcWhoo';
 
 // Initialize the Supabase Client
